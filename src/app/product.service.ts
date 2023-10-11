e55ad15e608e5306e4ede1427425ef377d5db47d
Dorra <dorra.ayari@esprit.tn>
2023-10-11 23:18:33 +0100

    modif

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   Products: Product[] = [
     { id: 1, code: 127, libelle: 'PC', prixunitaire: 2000, tauxTva: 10 },
     { id: 2, code: 128, libelle: 'PC', prixunitaire: 1000, tauxTva: 20 },
-    { id: 3, code: 129, libelle: 'PC', prixunitaire: 200, tauxTva: 30 },
+    { id: 3, code: 129, libelle: 'Table', prixunitaire: 200, tauxTva: 30 },
   ];
 
   constructor() {}
